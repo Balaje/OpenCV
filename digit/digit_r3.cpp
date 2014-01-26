c1a8b57a22faa4f73b3917630e1aa8534f755877
Balaje K <balaje6@gmail.com>
2014-01-26 12:20:48 +0530

    Skeletonization

--- digit/digit_r3.cpp
+++ digit/digit_r3.cpp
@@ -5,111 +5,67 @@
 using namespace cv;
 using namespace std;
 
-Mat edge(Mat &);
-Mat findContour(Mat &);
-Mat corners(Mat &);
-
-
 int main(int argc, char *argv[])
 {
-	Mat img, canny, contour, graph;
-	Point x, y;
-	namedWindow("Model",0);
-	
-	if(argc != 2)
+//Variable Declaration	
+	Mat img, canny, eroded;
+	namedWindow("Digit Model", CV_WINDOW_NORMAL);
+
+//Read Image
+	if(argc!=2)
 	{
-		cout<<"\nUsage ./digit_r3 <Your Image>";
+		cout<<"\nUsage: ./digit.cpp <Your Image>\n";
 		return -1;
 	}
 
 	img = imread(argv[1]);
-
-	canny = edge(img);
-	contour = findContour(canny);
-	graph = corners(contour);
-
-	imshow("Model", graph);
-	waitKey();
-}
-
-int findBiggestContour(vector<vector<Point> >contours)
-{
-	int index=-1;
-	int size=0;
-	for(int i=0; i<contours.size(); i++)
+	if(!img.data)
 	{
-		if(size < contours[i].size())
-		{
-			size=contours[i].size();
-			index=i;
-		}	
-	}	
-	return index;
-}
-
-Mat edge(Mat &img)
-{
-	Mat gray, canny, blur;
-
-	cvtColor(img, gray, CV_BGR2GRAY);	
-	int thresh = 120, aperture = 3;
-	GaussianBlur(gray, blur, Size(3,3), 0);
-	Canny(blur, canny, thresh, thresh*3, aperture);
-
-	return canny;
-}
-
-Mat findContour(Mat &canny)
-{
-	vector<vector<Point> > contours;
-	vector<Vec4i> h;
-
-	findContours(canny, contours, h, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
-
-	int s = findBiggestContour(contours);
-	vector<vector<Point> > poly(contours.size());
-	vector<Rect> boundRect(contours.size());
-
-	Mat boundaries = Mat::zeros(canny.size(), CV_8UC1);
-	Mat outline = Mat::zeros(canny.size(), CV_8UC1);
-	Mat pts = Mat::zeros(canny.size(), CV_8UC1);
-
-
-	if(s!=-1)
-	{
-		approxPolyDP( Mat(contours[s]), poly[s], 3, true );
-		boundRect[s] = boundingRect( Mat(poly[s]) );
-		rectangle(boundaries, boundRect[s].tl(), boundRect[s].br(), Scalar(255,255,255), 2, 8, 0);
-		drawContours(outline, contours, s, Scalar(255,255,255), 2, 8, h, 0, Point());
+		cout<<"\n Not a Valid Image\n";
+		return -1;
 	}
 
-	return outline;
-}	
+//edge
+	Mat gray, blur;
+	cvtColor(img, gray, CV_BGR2GRAY);
+	GaussianBlur(gray, blur, Size(3,3), 0);
+	Canny(blur, canny, 120, 360, 3);
 
-Mat corners(Mat &outline)
-{
-	Mat dst, dst_norm, dst_norm_scaled;
-	int thresh = 110;
+//contour
+	Mat bw;
+	canny.copyTo(bw);
+	vector<vector<Point> > contours;
+	vector<Vec4i> h;
+	findContours(canny, contours, h, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
+	for(int i=0; i<contours.size(); i++)
+	{
+		drawContours(bw, contours, i, Scalar(255,255,255), -1, 8, h, 0, Point());
+	}
 
-	cornerHarris( outline, dst, 2, 3, 0.04, BORDER_DEFAULT );
+//skeletonization
+	Mat  skel = Mat::zeros(img.size(), CV_8UC1);
+	Mat temp(img.size(), CV_8UC1);
+	Mat element = getStructuringElement(MORPH_CROSS, Size(3, 3));
 
-  	normalize( dst, dst_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat() );
-	convertScaleAbs( dst_norm, dst_norm_scaled );
+	threshold(bw, bw, 127, 255, cv::THRESH_BINARY);
 
-  	for( int j = 0; j < dst_norm.rows ; j++ )
-    { 
-    	for( int i = 0; i < dst_norm.cols; i++ )
-        {
-           if( (int) dst_norm.at<float>(j,i) > thresh )
-            {
-               circle( dst, Point( i, j ), 5,  Scalar(255,255,255), 2, 8, 0 );
-            }
-       }
-    }
+	bool done;
+	do
+	{
+  		morphologyEx(bw, temp, cv::MORPH_OPEN, element);
+  		bitwise_not(temp, temp);
+  		bitwise_and(bw, temp, temp);
+  		bitwise_or(skel, temp, skel);
+  		erode(bw, bw, element);
+ 	
+ 		double max;
+  		minMaxLoc(bw, 0, &max);
+  		done = (max == 0);
+	}while(!done);
 
-    return dst;
+//graph model
+		
+
+	imshow("Digit Model", skel);
+	waitKey();
 }
-
-
-
-
