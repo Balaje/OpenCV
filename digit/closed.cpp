db97605fd6a04d8a73abfba46feee24144f7b096
Balaje <balaje6@gmail.com>
2014-01-31 23:16:38 +0530

    Deleted unwanted images

--- digit/closed.cpp
+++ digit/closed.cpp
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
             return -1;
     }
 
-    //edge
+    //edge (Canny edge detection)
     Mat blur;
     cvtColor(img, gray, CV_BGR2GRAY);
     GaussianBlur(gray, blur, Size(3,3), 0);
@@ -32,13 +32,15 @@ int main(int argc, char *argv[])
     vector<vector<Point> > contours;
     vector<Vec4i> h;
     findContours(edge, contours, h, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
+    vector<Point> c;
 
     vector<vector<Point> > poly(contours.size());
     vector<Point2f> center (contours.size());
     vector<float> radius (contours.size());
 
-    int s= find(contours);
+    int s= find(contours);   //finds the biggest contour in the image
 
+//Part that calculates the number of closed curves
     int count=0;
     Point previous = center.back();
     int i=0;
@@ -48,22 +50,25 @@ int main(int argc, char *argv[])
         {
             approxPolyDP(Mat(contours[i]), poly[i] ,3, true);
             minEnclosingCircle((Mat)poly[i], center[i], radius[i]);
-            circle(img, center[i], radius[i], Scalar(255,0,0), 1, 8, 0);
-            count++;
+            count++;//increases contour count by one
             int dist = sqrt((previous.x-center[i].x)*(previous.x-center[i].x)+(previous.y-center[i].y)*(previous.y-center[i].y));
+            circle(img, center[i], radius[i], Scalar(255,0,0), 1, 8, 0);
             if(dist < 10)
             {
-                count--;
+                count--;   //if two contours are very close to each other, the contour count is reduced by 1
                 center.pop_back();
             }
-            line(img, center[i], Point(0,0), Scalar(0,0,255), 2, 8, 0);
-            imshow("Your Digit",img);
-            waitKey();
+            {
+                line(img, center[i], Point(0,0), Scalar(0,0,255), 2, 8, 0);
+                c.push_back(center[i]);   //contains the center of the contours, used to distinguish between 6/9
+            }
             previous = center[i];
             center.pop_back();
         }
     }
     printf("%d\n", count);
+
+    //predicts the result by comparing the number of closed curves
     if(count == 3)
     {
         printf("Possible digit is 8\n");
@@ -71,9 +76,15 @@ int main(int argc, char *argv[])
     if(count == 2)
     {
         printf("Possible digits are 6 or 9\n");
-        printf("Computing... \n");
+        if(c[0].y > c[1].y)
+            printf("Its a 9!!!\n");
+        else if(c[0].y < c[1].y)
+            printf("Its a 6!!!\n");
     }
-
+    if(count==1)
+    {
+        printf("Its a zero!!!\n");
+    }
     namedWindow("Your Digit", CV_WINDOW_NORMAL);
     imshow("Your Digit", img);
     waitKey();
